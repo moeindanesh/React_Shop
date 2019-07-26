9b34e9b8eaab1d4767804a8f2f0f85168b62ead2
MoeinDanA <moein.danesh97@gmail.com>
2019-07-26 10:51:54 +0430

    init shop

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Buttons from 'antd/es/button';
+import './App.css';
+
+export class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <Buttons type="primary">Button</Buttons>
+      </div>
+    )
+  }
+}
